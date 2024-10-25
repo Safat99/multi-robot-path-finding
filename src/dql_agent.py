c2d06f9812d54b4d0328d49d56b8c7c41b763171
Safat99 <sarkersafat99@gmail.com>
2024-10-24 21:12:30 -0600

    remember, act, replay comment added...

--- src/dql_agent.py
+++ src/dql_agent.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 class DQLAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
-        self.action_size = action_size
-        self.memory = []  # Experience replay memory
+        self.action_size = 8 # 8 types movement
+        self.memory = []  # Experience replay memory # a list or a queue
         self.gamma = 0.95  # Discount rate
         self.epsilon = 1.0  # Exploration rate
         self.epsilon_min = 0.01
@@ -21,21 +21,26 @@ class DQLAgent:
         return model
 
     def remember(self, state, action, reward, next_state, done):
+        """This method stores experiences (called transitions) in the agent's memory for replay later."""
         self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
-        if np.random.rand() <= self.epsilon:
+        """This method determines the next action the agent should take. It uses an epsilon-greedy policy:"""
+        if np.random.rand() <= self.epsilon: # Exploration
             return np.random.choice(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])
+        act_values = self.model.predict(state) # Exploitation
+        return np.argmax(act_values[0]) # rselect the action with the highest Q-value
 
     def replay(self, batch_size):
+        """This is where the actual learning happens. 
+        The agent randomly samples a minibatch of experiences from memory 
+        and trains the neural network to update the Q-values."""
         minibatch = np.random.choice(len(self.memory), batch_size)
         for i in minibatch:
             state, action, reward, next_state, done = self.memory[i]
             target = reward
             if not done:
-                target += self.gamma * np.amax(self.model.predict(next_state)[0])
+                target += self.gamma * np.amax(self.model.predict(next_state)[0]) # target Q-value
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
